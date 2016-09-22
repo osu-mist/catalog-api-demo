ed1c5ba99b317656df804bae99d2ed3b9f777072
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-09-22 10:12:38 -0700

    Modify views structure

--- catalog_api_demo/catalog_api_demo/views.py
+++ catalog_api_demo/catalog_api_demo/views.py
@@ -4,20 +4,26 @@ import json
 import requests
 
 
-def get_access_token():
-	config_file = open("../configuration.json")
-	config_data = json.load(config_file)
-
-	base_url   = config_data["hostname"] + config_data["version"] + config_data["api"]
-	token_url  = base_url + config_data["token_endpoint"]
-	grant_type = "client_credentials"
+def get_access_token(token_url, client_id, client_secret):
 	token_resp = requests.post(token_url, data={
-		'client_id': config_data["client_id"],
-		'client_secret': config_data["client_secret"],
-		'grant_type': grant_type})
+		'client_id': client_id,
+		'client_secret': client_secret,
+		'grant_type': 'client_credentials'})
+
 	return token_resp.json()['access_token']
 
 
 def catalog_api_demo(request):
-	print get_access_token()
-	return render_to_response('catalog_api_demo.html')
+	config_file   = open("../configuration.json")
+	config_data   = json.load(config_file)
+	base_url      = config_data['hostname'] + config_data['version'] + config_data['api']
+	token_url     = base_url + config_data['token_endpoint']
+	client_id     = config_data['client_id']
+	client_secret = config_data['client_secret']
+	access_token  = get_access_token(token_url, client_id, client_secret)
+
+	headers = {'Authorization': 'Bearer ' + access_token}
+	request_url = base_url + '/terms'
+	response = requests.get(request_url, headers=headers).json()
+
+	return render_to_response('catalog_api_demo.html', locals())
