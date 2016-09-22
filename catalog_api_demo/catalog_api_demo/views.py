b738fc242c0ab55813f09f7d3a00d353f1c2da91
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-09-22 14:40:07 -0700

    Minor fix

--- catalog_api_demo/catalog_api_demo/views.py
+++ catalog_api_demo/catalog_api_demo/views.py
@@ -11,7 +11,6 @@ def get_access_token(token_url, client_id, client_secret):
 		'client_id': client_id,
 		'client_secret': client_secret,
 		'grant_type': 'client_credentials'})
-
 	return token_resp.json()['access_token']
 
 
@@ -19,14 +18,13 @@ def catalog_api_demo(request):
 	config_file   = open('../configuration.json')
 	config_data   = json.load(config_file)
 	base_url      = config_data['hostname'] + config_data['version'] + config_data['api']
+	request_url   = base_url
 	token_url     = base_url + config_data['token_endpoint']
 	client_id     = config_data['client_id']
 	client_secret = config_data['client_secret']
 	access_token  = get_access_token(token_url, client_id, client_secret)
+	headers       = {'Authorization': 'Bearer ' + access_token}
 
-	headers = {'Authorization': 'Bearer ' + access_token}
-
-	request_url = base_url
 	if request.method == 'POST':
 		form = CourseForm(request.POST)
 		if form.is_valid():
