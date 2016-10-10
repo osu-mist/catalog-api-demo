bd45d47e0a44b9d4de169f3e56bd92f5153b6751
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-10-10 14:19:39 -0700

    Update template

--- catalog_api_demo/catalog_api_demo/forms.py
+++ catalog_api_demo/catalog_api_demo/forms.py
@@ -16,8 +16,8 @@ class CourseForm(forms.Form):
 class TermForm(forms.Form):
 	term_choices = (('fall', 'Fall'), ('winter', 'Winter'), ('spring', 'Spring'), ('summer', 'Summer'), ('none', 'None'))
 
+	is_open   = forms.BooleanField(label='Open', required=False)
 	year      = forms.CharField(label='Year', required=False)
 	term      = forms.ChoiceField(label='Term', widget=forms.RadioSelect, choices=term_choices, required=False)
-	is_open   = forms.BooleanField(label='Open', required=False)
 	page_size = forms.CharField(label='Page Size', required=False)
 	page_num  = forms.CharField(label='Page Number', required=False)
